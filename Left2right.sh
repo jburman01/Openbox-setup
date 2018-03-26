e819f18d28a4b82f7f365ef5627e3591dd7a8010
jburman01 <37814024+jburman01@users.noreply.github.com>
2018-03-26 13:03:33 -0700

    Add files via upload

---
+++ Left2right.sh
@@ -0,0 +1,3 @@
+#Add me to you .screenlaytout folder
+#!/bin/sh
+xrandr --output VIRTUAL1 --off --output DP3 --off --output eDP1 --mode 1600x900 --pos 0x180 --rotate normal --output DP1 --off --output HDMI3 --off --output HDMI2 --mode 1920x1080 --pos 3520x0 --rotate normal --output HDMI1 --off --output VGA1 --mode 1920x1080 --pos 1600x0 --rotate normal --output DP2 --off
